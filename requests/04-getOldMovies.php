fa0dc3ba3c753ec560dabd805933ab7ef8dfcd3a
AnthonyR <86236227+anthonyr-14051997@users.noreply.github.com>
2022-06-08 12:33:08 +0200

    modif 04

--- requests/04-getOldMovies.php
+++ requests/04-getOldMovies.php
@@ -4,7 +4,7 @@
 
 require_once('../connect.php');
 
-$req_select_before = "SELECT ";
+$req_select_before = "SELECT title, release_years_id, release_year FROM infos_movies INNER JOIN release_years ON release_years.id = infos_movies.release_years_id WHERE release_years.release_year < 2002";
 $req_get_before = $db->query($req_select_before);
 
 while ($a = $req_get_before->fetch(PDO::FETCH_ASSOC)) {
